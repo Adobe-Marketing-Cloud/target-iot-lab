dc9003e7bddb9ed1483f3c9351d9e44161c3b4cd
Nipun Nair <nipun.nair@gmail.com>
2016-02-28 16:22:33 -0800

    Removing old files

--- wellness-demo/README.md
+++
@@ -1,1 +0,0 @@
-# adobesummit2015
--- wellness-demo/js/site.js
+++
@@ -1,9 +0,0 @@
-function getMbox3rdPartyIdParameter() {
-    var hash = $.cookie('mbox3rdPartyId');
-
-    if (hash) {
-        return "mbox3rdPartyId=" + hash;      
-    } else {
-        return "";
-    }   
-}
